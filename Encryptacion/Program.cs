3e6fa7ccb8324c604c71ce30e1fe957e53b44a23
marcoscabrera <marcoscabrera021@gmail.com>
2023-08-21 01:04:04 -0300

    Implementacion de Encryptacion con SHA256

---
+++ Encryptacion/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encryptacion
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            using (Models.MiSistemaEntities db = new Models.MiSistemaEntities())
+            {
+                try
+                {
+                    Usuarios_DB usuarios_DB = new Usuarios_DB();
+
+                    foreach (var item in usuarios_DB.Read())
+                    {
+                        usuarios_DB.Edit(item.id, Encrypt.GetSHA2S6(item.password));
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+            }
+
+
+        }
+    }
+}
---
+++ Encryptacion/Usuarios_DB.cs
@@ -0,0 +1,70 @@
+﻿using Encryptacion.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encryptacion
+{
+    public class Usuarios_DB
+    {
+        private string connectionString =
+            "Data Source = .; Database = MiSistema; Trusted_Connection = True; " +
+            "User = sa; Password = 123456;";
+
+        public List<usuario> Read()
+        {
+            
+            List<usuario> usuarios = new List<usuario>();
+
+            string query = "select id, password from usuario ";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+
+                connection.Open();
+
+                var reader = command.ExecuteReader();
+
+                while (reader.Read()) 
+                {
+                    var item = new usuario();
+                    item.id = reader.GetInt32(0);
+                    item.password = reader.GetString(1);
+
+                    usuarios.Add(item);
+                }
+
+                connection.Close();
+                reader.Close();
+            }
+
+            return usuarios;
+        }
+
+        public void Edit( int id, string password)
+        {
+            string query = "UPDATE usuario set password=@password " +
+                            " where id=@id ";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+
+                command.Parameters.AddWithValue("@id", id);
+                command.Parameters.AddWithValue("@password", password);
+
+                connection.Open();
+
+                command.ExecuteNonQuery();
+
+                connection.Close();
+            }
+        }
+    }
+
+}
